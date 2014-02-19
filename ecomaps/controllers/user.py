54ba735ed5123323032caba30543437e49f5215c
Chirag Mistry <chirag.mistry@tessella.com>
2014-02-19 15:05:57 +0000

    Added the functionality to add a new user.

--- ecomaps/controllers/user.py
+++ ecomaps/controllers/user.py
@@ -5,6 +5,8 @@ from ecomaps.services.user import UserService
 from ecomaps.services.dataset import DatasetService
 from pylons import tmpl_context as c
 from formencode import htmlfill
+import formencode
+from ecomaps.model.create_new_user_form import CreateUserForm
 
 __author__ = 'Chirag Mistry'
 
@@ -27,7 +29,47 @@ class UserController(BaseController):
         self._dataset_service = dataset_service
 
     def view_users(self):
-
+        """Allow user to see all users of the system
+        """
         c.all_users = self._user_service.get_all_users()
 
         return render('list_of_users.html')
+
+    def create(self):
+        """Create a new user
+        """
+        if not request.POST:
+
+            return render('new_user.html')
+
+        schema = CreateUserForm()
+        c.form_errors = {}
+
+        if request.POST:
+
+            try:
+                c.form_result = schema.to_python(request.params)
+
+            except formencode.Invalid, error:
+
+                c.form_result = error.value
+                c.form_errors = error.error_dict or {}
+                html = render('new_user.html')
+                return htmlfill.render(html,
+                                       defaults=c.form_result,
+                                       errors=c.form_errors,
+                                       auto_error_formatter=custom_formatter)
+            else:
+
+                self._user_service.create(c.form_result.get('name'),
+                                          c.form_result.get('username'),
+                                          c.form_result.get('email'),)
+                c.all_users = self._user_service.get_all_users()
+                return render('list_of_users.html')
+
+
+def custom_formatter(error):
+    """Custom error formatter"""
+    return '<span class="help-inline">%s</span>' % (
+        htmlfill.html_quote(error)
+    )
