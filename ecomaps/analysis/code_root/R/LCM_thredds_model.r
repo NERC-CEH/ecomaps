d91be6142ca8180418556837a3c9a795d42b3e4a
Phil Jenkins <jenp@tessella.com>
2014-03-19 12:54:07 +0000

    Added latest R changes, time slicing compatible

--- ecomaps/analysis/code_root/R/LCM_thredds_model.r
+++ ecomaps/analysis/code_root/R/LCM_thredds_model.r
@@ -5,7 +5,7 @@ require(ncdf)
 require(ncdf4)
 require(gdata)
 
-##progress_rep is constantly updated to specify what part of the code we are up to
+##progress_fn is constantly updated to specify what part of the code we are up to
 progress_fn <- function(message) {
 
     if(!file.exists(progress_rep_file)) {
@@ -25,6 +25,16 @@ do_work <- function() {
     options(contrasts = c(factor = "contr.SAS",ordered = "contr.poly"))
 
     #specify the file server and file to pull the netdcdf file off  and the variables needed from each
+    #covariate_data = list()
+    #covariate_data[[1]]=list()
+    #covariate_data[[1]]$linkfile="http://thredds-prod.nerc-lancaster.ac.uk/thredds/fileServer/LCM2007_1kmDetail/LCM2007_GB_1K_DOM_TAR.nc"
+    #covariate_data[[1]]$vars=c("LandCover")
+    #covariate_data[[1]]$tm_slice=NA
+    #covariate_data[[2]]=list()
+    #covariate_data[[2]]$linkfile="http://thredds-prod.nerc-lancaster.ac.uk/thredds/fileServer/CHESSModel001Run001OutputDetail/CHESS_MODEL001_RUN001_OUT_1971-01.nc"
+    #covariate_data[[2]]$vars=c("ta","precip")
+    #covariate_data[[2]]$tm_slice=c(1,31)
+
     covariate_data = list()
 
     # PJ: Constructing the named list dynamically based on what
@@ -35,61 +45,38 @@ do_work <- function() {
         url = names(coverage_setup)[i]
         covariate_data[[i]]$linkfile = url
         covariate_data[[i]]$vars = c(coverage_setup[[url]])
-
-        #progress_fn(paste("new ", covariate_data))
-        #stop("Test Complete")
     }
-
-    old_covariate_data = list()
-    old_covariate_data[[1]]=list()
-    old_covariate_data[[1]]$linkfile="http://localhost:8080/thredds/fileServer/testAll/CHESSAnnualTotalPrecip.nc"
-    old_covariate_data[[1]]$vars=c("precip")
-    #covariate_data[[2]]=list()
-    #covariate_data[[2]]$linkfile="http://thredds-prod.nerc-lancaster.ac.uk/thredds/fileServer/CHESSModel001Run001OutputDetail/CHESS_MODEL001_RUN001_OUT_1971-01.nc"
-    #covariate_data[[2]]$vars=c("ta","precip")
-
-    #progress_fn(paste("old ", old_covariate_data))
-    #stop("Test complete")
-
-    #capture the user's name and email to include in netcdf file produced
-    users_name = user_name
-    users_email = email_address
 
     #read in concatonated point data (simon's python script does this)
     dat=read.csv(csv_file)
 
+    #user defined inputs. mult year defines if temporal correlation needed. rand_grp defines if random effects needed.
+    mult_year <- "year" ; rand_grp <- "SERIES_NUM" ; data_type <- "Cont" ; model_variable="loi"
 
-     #user defined inputs. mult year defines if temporal correlation needed. rand_grp defines if random effects needed.
-     mult_year <- "year" ; rand_grp <- "SERIES_NUM" ; data_type <- "Cont" ; model_variable="loi"
+    ## define the family of distributions to choose from
+    poss_fam <- c("gaussian","Gamma","quasibinomial","quasipoisson")
 
+    #create new dataset so that we do not over-write the input one
+    newdat=dat
 
-     ## define the family of distributions to choose from
-     poss_fam <- c("gaussian","Gamma","quasibinomial","quasipoisson")
+    ## specify the response variable to include in the models
+    newdat$response <- newdat[,which(names(newdat)==model_variable)]
+    if(!is.null(rand_grp)){newdat$rnd_group <- newdat[,which(names(newdat)==rand_grp)]}
+    if(!is.null(mult_year)){newdat$tm_var <- newdat[,which(names(newdat)==mult_year)]}
 
+    progress_fn("Checking distribution of Response Variable")
 
-     #create new dataset so that we do not over-write the input one
-     newdat=dat
-
-     ## specify the response variable to include in the models
-     newdat$response <- newdat[,which(names(newdat)==model_variable)]
-     if(!is.null(rand_grp)){newdat$rnd_group <- newdat[,which(names(newdat)==rand_grp)]}
-     if(!is.null(mult_year)){newdat$tm_var <- newdat[,which(names(newdat)==mult_year)]}
-
-     progress_fn("Checking distribution of Response Variable")
-
-     #test normality of response if it is continuos.
-     if(data_type=="Cont"){
+    #test normality of response if it is continuos.
+    if(data_type=="Cont"){
         norm.test.res <- shapiro.test(newdat$response)
 
-       if(norm.test.res$p.value<0.05){
-           data_type="ContNonG"
+        if(norm.test.res$p.value<0.05){
+            data_type="ContNonG"
         }
-
      }
 
-     #select the distribution family to use given the type of data being modelled
-     data_fam <- switch(data_type,"Cont"=poss_fam[1],"ContNonG"=poss_fam[2],"Count"=poss_fam[4],"Binary"=poss_fam[3])
-
+    #select the distribution family to use given the type of data being modelled
+    data_fam <- switch(data_type,"Cont"=poss_fam[1],"ContNonG"=poss_fam[2],"Count"=poss_fam[4],"Binary"=poss_fam[3])
 
     progress_fn("Loading data into R")
 
@@ -103,75 +90,51 @@ do_work <- function() {
         #download the files locally to enable easy reading in
         download.file(url=covariate_data[[i]]$linkfile,destfile=temp_netcdf_file, mode="wb")
 
-        ##cov_dat <- open.ncdf(temp_netcdf_file)
         cov_dat <- nc_open(temp_netcdf_file)
 
         for(vn in 1:length(covariate_data[[i]]$vars)){
 
+            progress_fn("In here!")
             nm_var[cn] <- covariate_data[[i]]$vars[vn]
 
             #save the spatial dimensions
-            ##north[[cn]] <- get.var.ncdf(cov_dat,"y")
+            #north[[cn]] <- get.var.ncdf(cov_dat,"y")
             north[[cn]] <- ncvar_get(cov_dat, "y")
             n_north[cn] <- dim(north)
-
-            ##east[[cn]] <- get.var.ncdf(cov_dat,"x")
+            #east[[cn]] <- get.var.ncdf(cov_dat,"x")
             east[[cn]] <- ncvar_get(cov_dat, "x")
             n_east[cn] <- dim(east)
 
             spat_res[cn] <- abs(mean(diff(north[[cn]])))
 
-            # Need to check the dimensionality of the dataset before we grab the data
-            var_data <- cov_dat$var[[vn]]
-
-            if( var_data$ndims > 2 ) {
-
-
-                ##TODO: Use variable passed in instead
-                time_point <- 13
-                ndims <- var_data$ndims
-                shape <- var_data$varsize
-                n_time_points <- shape[ndims]
-
-                start <- rep(1, ndims)
-
-                # Use the time point passed in
-                start[ndims] <- time_point
-                count <- shape
-                # We only want one slice through time
-                count[ndims] <- 1
-
-                tmp.array[[cn]] <- ncvar_get(cov_dat, var_data, start=start, count=count)
-            }
-            else {
-                tmp.array[[cn]] <- ncvar_get(cov_dat, nm_var[cn])
-            }
-
             #Next, the data are read using the get.var.ncdf()function, and some �attributes� are read, like the long name of the variable and its missing value.  Then the missing values in the data array are replaced by R/S+ "data not available" values.
 
             # get the data and attributes
-            ##tmp.array[[cn]] <- get.var.ncdf(cov_dat,nm_var[cn])
+            tmp.array[[cn]] <- ncvar_get(cov_dat,nm_var[cn])
+
+            if(!is.na(time_slices[[nm_var[cn]]])){
+                progress_fn(paste("Time slice for: ", nm_var[cn], ": ",  time_slices[[nm_var[cn]]]))
+                tmp.array[[cn]] <- tmp.array[[cn]][,,(time_slices[[nm_var[cn]]])]
+            }
 
             ###retrieve meta-data from netcdf files
 
             #dlname <- att.get.ncdf(lcm1km07,nm_var,"long_name")$value
-            ##dunits[cn] <- att.get.ncdf(cov_dat,nm_var[cn],"units")$hasatt
+            #dunits[cn] <- att.get.ncdf(cov_dat,nm_var[cn],"units")$hasatt
             dunits[cn] <- ncatt_get(cov_dat, nm_var[cn], "units")$hasatt
 
-            ##fillvalue <- att.get.ncdf(cov_dat,nm_var[cn],"_FillValue")
+            #fillvalue <- att.get.ncdf(cov_dat,nm_var[cn],"_FillValue")
             fillvalue <- ncatt_get(cov_dat, nm_var[cn], "_FillValue")
 
             # replace fillvalues with NAs
             tmp.array[[cn]][tmp.array[[cn]]==fillvalue$value] <- NA
 
             cn <- cn +1
-
         }
 
         # done with the netCDF file, so close it
-        ##close.ncdf(cov_dat)
+        #close.ncdf(cov_dat)
         nc_close(cov_dat)
-
     }
 
     progress_fn("Constructing Model formula")
@@ -206,7 +169,6 @@ do_work <- function() {
 
       v=which(apply(x,2,function(X){length(unique(X))})==dim(x)[1])
       w=which(apply(y,2,function(X){length(unique(X))})<(dim(y)[1]-3))
-
       len_id = apply(newdat[,which(is.element(names(newdat),intersect(names(v),names(w))))],2,function(X){length(unique(X))})
       tmp_nm = names(len_id)[which.max(len_id)]
       newdat$temp_rand = newdat[,which(names(newdat)==tmp_nm)]
@@ -219,8 +181,8 @@ do_work <- function() {
         if(is.null(rand_grp) & !is.null(mult_year)){
           mod=try(gamm(as.formula(form),correlation=corAR1(form=~tm_var|temp_rand),data=newdat,family=data_fam,niterPQL=5))
         }else{
-            progress_fn("Applying GAMM, multi-year and grouped")
           if(!is.null(rand_grp) & !is.null(mult_year)){
+            progress_fn("Applying GAMM, multi-year and grouped")
             mod=try(gamm(as.formula(form),random=list(temp_rand=~1),correlation=corAR1(form=~tm_var|temp_rand),data=newdat,family=data_fam,niterPQL=5))
             if(class(mod)=="try-error"){
               mod=try(gamm(as.formula(form),random=list(rnd_group=~1),data=newdat,family=data_fam,niterPQL=5))
@@ -241,7 +203,6 @@ do_work <- function() {
         std_res <- spat_res[resuse]
         scale_res <- spat_res/std_res
 
-        progress_fn("Converting to common scale")
         for(k in which(scale_res!=1)){
 
             mat=matrix(1:(length(east[[k]])*length(north[[k]])),ncol=length(north[[k]]),nrow=length(east[[k]]),byrow=TRUE)
@@ -252,15 +213,10 @@ do_work <- function() {
                    x=rep(mat[j,],rep(scale_res[k],length(mat[j,])))
                    y=rep(x,scale_res[k])
                    z=c(z,y)
-
-
             }
 
             tmp.array[[k]] = matrix(unmatrix(tmp.array[[k]],byrow=TRUE)[z],byrow=TRUE,ncol=1300,nrow=700)
-
         }
-
-
 
          ##write model results to the assocaited values
 
@@ -268,7 +224,6 @@ do_work <- function() {
 
           #### predict model
           pred_points <- expand.grid(north[[resuse]],east[[resuse]])
-
           newd=data.frame(easting=pred_points[,2],northing=pred_points[,1])
 
           for(cov_i in 1:length(tmp.array)){
@@ -280,23 +235,21 @@ do_work <- function() {
           ##set any values outside the covariate space to NA. Particularly important for factors
 
           for(fv in factor_vars){
-
             c1=which(names(newd)==fv) ; c2=which(names(newdat)==fv)
             idx=which(!is.element(newd[,c1],unique(newdat[,c2])))
-
-            if(!is.null(idx)) {
-                newd[idx,c1]=NA
-            }
+            newd[idx,c1]=NA
             newd[,c1]=as.factor(as.character(newd[,c1]))
-
           }
 
 
           ##use the model the predict the coverage data. ensure model predict the standard error as well - this may be replaced by bootstrap in future
           modpred=predict(mod$gam,newdata=newd,se.fit=TRUE,type="response")
+
           pred_se_tab=data.frame(preds=modpred$fit,sefit=modpred$se.fit)
 
-          #pred_se_tab[idx,]=NA
+          if(exists("idx")) {
+            pred_se_tab[idx,]=NA
+          }
 
           names(pred_se_tab)=c("Predicted","Standard_Error")
 
@@ -309,35 +262,47 @@ do_work <- function() {
         #####png(height=720,width=700,file="map_output.png")
         #####png(height=720,width=700,file="N://SMW//Python//EcoMaps//Python//20140213//images//map_output.png")
 
+        progress_fn("Writing map image")
         png(height=720,width=700,file=map_image_file)
           par(mfrow=c(1,2))
           brk=c(seq(0,50,by=10),100)
           cols=c("lightgoldenrod1","lightgoldenrod3","burlywood3","darkgoldenrod","lightsalmon4","darkorange4")
           par(mai=c(0,0,0.4,0));image(east[[resuse]],rev(north[[resuse]]),(out_mat_pred[,1300:1]),asp=1,main="",col=cols,xaxt="n",breaks=brk,xlab="",yaxt="n",ylab="")
+           lb=c()
+           labs=as.character((brk)[-length(brk)])
+           for(i in 1:length(labs)){
+                 lb[i]=paste(as.character(labs[i])," - ",brk[i+1])
+           }
+           legend("topright",legend=as.character(lb),col=cols,pch=15,pt.cex=2.4,bty="n",title=model_variable,cex=0.75)
+
 
           brk=c(seq(1,3,length=10),100)
           cols=rev(grey(seq(0.1,0.9,len=10)))
           par(mai=c(0,0,0.4,0));image(east[[resuse]],rev(north[[resuse]]),(out_mat_var[,1300:1]),asp=1,main="",col=cols,xaxt="n",breaks=brk,xlab="",yaxt="n",ylab="")
+          lb=c()
+          labs=as.character(round(brk,2)[-length(brk)])
+          for(i in 1:length(labs)){
+                lb[i]=paste(as.character(labs[i])," - ",round(brk[i+1],2))
+          }
+          legend("topright",legend=as.character(lb),col=cols,pch=15,pt.cex=2.4,bty="n",title=paste("Variance of ",model_variable,sep=""),cex=0.75)
+
+
         dev.off()
 
-
+        progress_fn("Writing fit image")
         png(height=320,width=700,file=fit_image_file)
           par(mfrow=c(1,2))
           plot(mod$gam$y,mod$gam$fitted.values,xlab="Observed Values",ylab="Fitted Values")
           abline(a=0,b=1,col="red",lwd=2)
           hist(mod$gam$y-mod$gam$fitted.values,main="",xlab="Model Residuals")
 
-        progress_fn("Result images written")
         dev.off()
-        progress_fn("Calculating AIC")
         if(mod_t=="norm_gam"){aic_val <- AIC(mod$gam)}else{aic_val <- AIC(mod$lme)}
-        progress_fn("AIC Complete, calculation root mean sq")
         rmse <- sqrt(mean((mod$gam$y-mod$gam$fitted.values)^2))
-        progress_fn("RMS complete, calculating r squared")
         r2 <- (summary(mod$gam))$r.sq
-        progress_fn("R squared complete, writing formula")
         mod_formula <- paste(model_variable," ~ Intercept + ",paste(nm_var,collapse=" + "),sep="")
-        progress_fn("Formula complete")
+        mod_sum <- summary(mod$gam)
+
 
     }else{
 
@@ -369,8 +334,6 @@ do_work <- function() {
 
     }
 
-
-
     ##  Write out the predicted LOI and variance to a single netCDF file.
     ##  Predicted LOI and variance will be written out as netCDF variables, with variance specified as an acillary variable
     ##  (as per the CF 1.6 Convention; see:  http://cf-pcmdi.llnl.gov/documents/cf-conventions/1.6/cf-conventions.html#ancillary-data).
@@ -384,7 +347,6 @@ do_work <- function() {
     download.file(url=fl,destfile=temp_netcdf_file, mode="wb")
 
     #####cov_dat <- open.ncdf("N://SMW//Python//EcoMaps//Python//20140213//netCDF//temp.nc")
-    ##cov_dat <- open.ncdf(temp_netcdf_file)
     cov_dat <- nc_open(temp_netcdf_file)
 
     y <- ncdim_def("y", "m", north[[resuse]], unlim=FALSE, create_dimvar=TRUE, longname="northing - OSGB36 grid reference")
@@ -393,7 +355,6 @@ do_work <- function() {
     pred <- ncvar_def(name=model_variable, units="PercentLOI", dim=list(x, y), missval=(-999), longname="Topsoil LOI Estimate from CS data")
     mod_var <- ncvar_def(name="VAR", units="PercentLOI", dim=list(x, y), missval=(-999), longname="Topsoil LOI Estimate Variance from CS data")
     prj <- ncvar_def(name="transverse_mercator", units="", dim=list(), longname="coordinate_reference_system", prec="integer")
-
 
     #####ncnew <- nc_create("N://SMW//Python//EcoMaps//Python//20140213//netCDF//soil_loi.nc", list(soil_loi, soil_var, prj))
     ncnew <- nc_create(output_netcdf_file, list(pred, mod_var, prj))
@@ -431,11 +392,11 @@ do_work <- function() {
     ncatt_put(ncnew,"transverse_mercator", "false_northing" ,-100000.0,prec="double")
     ncatt_put(ncnew,"transverse_mercator", "scale_factor_at_projection_origin",0.9996012717,prec="double")
 
-    ncatt_put(ncnew, 0, "title", "A test map of soil carbon loi")
+    ncatt_put(ncnew, 0, "title", paste("A map of ",model_variable,sep=""))
     ncatt_put(ncnew, 0, "institution", "Centre for Ecology & Hydrology (CEH) Lancaster")
     ncatt_put(ncnew, 0, "source", "Centre for Ecology & Hydrology (CEH) Lancaster")
     ncatt_put(ncnew, 0, "reference", "EcoMAPS v1.01")
-    ncatt_put(ncnew, 0, "description", paste("Predicted map of ",model_variable,sep=""))
+    ncatt_put(ncnew, 0, "description", paste("Predicted map of ",model_variable," using EcoMAPS v1.01",sep=""))
     ncatt_put(ncnew, 0, "grid_mapping", "transverse_mercator")
     ncatt_put(ncnew, 0, "history", "history")
     ncatt_put(ncnew, 0, "summary", paste("Predicted map of ",model_variable,sep=""))
@@ -444,42 +405,30 @@ do_work <- function() {
     ncatt_put(ncnew, 0, "date_modified", as.character(Sys.Date()), prec="text")
     ncatt_put(ncnew, 0, "date_issued", as.character(Sys.Date()), prec="text")
 
-    ncatt_put(ncnew, 0, "creator_name", users_name)
-    ncatt_put(ncnew, 0, "creator_url", "http://www.ceh.ac.uk/staffwebpages/drpeterhenrys.html")
-    ncatt_put(ncnew, 0, "creator_email", users_email)
+    ncatt_put(ncnew, 0, "creator_name", user_name)
+    ncatt_put(ncnew, 0, "creator_url", "http://www.ceh.ac.uk//")
+    ncatt_put(ncnew, 0, "creator_email", email_address)
 
-    #ncatt_put(ncnew, 0, "geospatial_lon_min", att.get.ncdf(cov_dat, 0, "geospatial_lon_min")$value, prec="double")
     ncatt_put(ncnew, 0, "geospatial_lon_min", ncatt_get(cov_dat, 0, "geospatial_lon_min")$value, prec="double")
-
-    #ncatt_put(ncnew, 0, "geospatial_lat_min", att.get.ncdf(cov_dat, 0, "geospatial_lat_min")$value, prec="double")
     ncatt_put(ncnew, 0, "geospatial_lat_min", ncatt_get(cov_dat, 0, "geospatial_lat_min")$value, prec="double")
-
-    #ncatt_put(ncnew, 0, "geospatial_lon_max", att.get.ncdf(cov_dat, 0, "geospatial_lon_max")$value, prec="double")
     ncatt_put(ncnew, 0, "geospatial_lon_max", ncatt_get(cov_dat, 0, "geospatial_lon_max")$value, prec="double")
-
-    #ncatt_put(ncnew, 0, "geospatial_lat_max", att.get.ncdf(cov_dat, 0, "geospatial_lat_max")$value, prec="double")
     ncatt_put(ncnew, 0, "geospatial_lat_max", ncatt_get(cov_dat, 0, "geospatial_lat_max")$value, prec="double")
-
-    #ncatt_put(ncnew, 0, "licence", att.get.ncdf(cov_dat,0,"licence")$value)
     ncatt_put(ncnew, 0, "licence", ncatt_get(cov_dat,0,"licence")$value)
-
     ncatt_put(ncnew, 0, "publisher_name", "Centre for Ecology & Hydrology")
     ncatt_put(ncnew, 0, "publisher_url", "http://www.ceh.ac.uk")
     ncatt_put(ncnew, 0, "publisher_email", "enquiries@ceh.ac.uk")
-    #ncatt_put(ncnew, 0, "Conventions", att.get.ncdf(cov_dat, 0, "Conventions")$value)
     ncatt_put(ncnew, 0, "Conventions", ncatt_get(cov_dat, 0, "Conventions")$value)
     ncatt_put(ncnew, 0, "comment", "comment")
     ncatt_put(ncnew, 0, "model_formula", mod_formula)
     ncatt_put(ncnew, 0, "root_mean_square_error", rmse)
     ncatt_put(ncnew, 0, "r_squared", r2)
     ncatt_put(ncnew, 0, "AIC", aic_val)
-    ncatt_put(ncnew, 0, "Model_Terms", paste(c(row.names(x$pTerms.table),row.names(x$s.table)),collapse=","))
-    ncatt_put(ncnew, 0, "Model_pVals", paste(round(c((x$pTerms.table[,3]),(x$s.table[,4])),4),collapse=","))
+    ncatt_put(ncnew, 0, "Model_Terms", paste(c(row.names(mod_sum$pTerms.table),row.names(mod_sum$s.table)),collapse=","))
+    ncatt_put(ncnew, 0, "Model_pVals", paste(round(c((mod_sum$pTerms.table[,3]),(mod_sum$s.table[,4])),4),collapse=","))
     nc_close(ncnew)
 }
 #########################
 #########################
 
-tryCatch(do_work(), error=function(e) { progress_fn(paste(geterrmessage(), traceback())); return(NA)})
-
-
+#do_work()
+tryCatch(do_work(), error=function(e) { progress_fn(paste(geterrmessage(), sys.calls())); return(NA)})
