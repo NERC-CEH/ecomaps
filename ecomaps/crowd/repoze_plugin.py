5472ec20e26013bcc5e430942c96e1cbe282b613
Christopher Johnson <cjohn@ceh.ac.uk>
2015-03-18 11:14:20 +0000

    removing any cookie setting logic from the crowd repoze_plugin. Powering the authentication logic off of a proxy supplied http header (a la LWIS)

--- ecomaps/crowd/repoze_plugin.py
+++ ecomaps/crowd/repoze_plugin.py
@@ -1,5 +1,6 @@
 import logging
 from ecomaps.crowd.client import CrowdClient, ClientException
+from ecomaps.services.user import UserService
 from zope.interface import directlyProvides
 from repoze.who.interfaces import IChallengeDecider
 
@@ -23,47 +24,23 @@ class CrowdRepozePlugin(object):
 
         # Set up the bits we need
         self._client = CrowdClient()
-        self._cookie_name = 'gwgj9gj38g9d'
+        self._user_service = UserService()
 
     def authenticate(self, environ, identity):
-        """Authenticates the user supplied against Crowd
+        """Authenticates a preauthenticated user against Crowd
             Params:
                 environ: The WSGI environment
-                identity: Information about the user,
-                            which may include login/password
-                             or login/token if a session is active
+                identity: Identity dict which contains the username and
+                                preauth set to true
             Returns:
                 Name of successfully authenticated user, or None
         """
-        try:
+        log.debug("--> Repoze authenticate")
 
-            log.debug("--> Repoze authenticate")
-
-            # Do we have an active session already?
-            if 'token' in identity:
-
-                log.debug("--> Already got a token, returning %s" % identity['login'])
-
-                # If so, we can just return the user name
-                return identity['login']
-
-            # Otherwise, it's a trip to Crowd to authenticate
-
-            log.debug("--> No token")
-            username = identity['login']
-            password = identity['password']
-
-            user = self._client.check_authenticated(username, password)
-
-            log.debug("--> Auth result %s" % user)
-
-            self._user = user
-            return user['name']
-
-        except KeyError:
-            return None
-        except ClientException as ex:
-            log.error("Error authenticating %s with Crowd: %s" % (identity['login'],ex))
+        # Do we have an active session already?
+        if 'preauth' in identity:
+            return identity['login']
+        else:
             return None
 
     def add_metadata(self, environ, identity):
@@ -72,35 +49,20 @@ class CrowdRepozePlugin(object):
         pass
 
     def identify(self, environ):
-        """Verifies the user's identity in crowd
+        """Reads the supplied HTTP_REMOTE_USER header from an authentication proxy and
+            verifies the user's identity in crowd
             Params:
                 environ: The WSGI environment
             Returns:
-                A dictionary containing 'login' and 'token' entries if
+                A dictionary containing 'login' and 'preauth' entries if
                 a valid session is active, otherwise None.
         """
-
-        # Do we have an active session?
-        token = self._get_token_value(environ)
-
-        if not token:
-            return None
-
-        try:
-
-            log.debug("Checking user session with token: %s" % token)
-            # Verify the session is still valid...
-            user_obj = self._client.verify_user_session(token)
-
-            log.debug("User OK: %s" % user_obj)
-            # It is - so return the expected data structure
+        if 'HTTP_REMOTE_USER' in environ:
             return {
-                "login": user_obj['user']['name'],
-                "token": user_obj['token']
+                "login":   environ['HTTP_REMOTE_USER'],
+                "preauth": True
             }
-        except ClientException:
-            # Session is invalid or deleted
-            log.debug("User session no longer valid")
+        else:
             return None
 
     def remember(self, environ, identity):
@@ -112,72 +74,26 @@ class CrowdRepozePlugin(object):
         """
         username = identity['login']
 
-        # We have a token passed in already
-        if 'token' in identity:
-            token = identity['token']
-
-            # Make sure it is still valid, kick out if not
-            if not self.identify(environ):
-                return None
-        else:
-            # No token, so username and password should be present
-            password = identity['password']
-
-            # What IP is the user requesting from?
-            remote_address = environ['REMOTE_ADDR']
-
-            # Ask Crowd for a session, which gives us a token ID
-            user_object = self._client.create_user_session(username, password, remote_address)
-            token = user_object['token']
-
-            # Now pull out the user information
+        # Loaded in the details of this user from crowd. Lets check if they need to be
+        # stored in the local database
+        u = self._user_service.get_user_by_username(username)
+        if not u:
+            log.debug("Couldn't find %s in Ecomaps DB, creating user" % username)
             user_info = self._client.get_user_info(username)
-
-            # We'll use these in case we need to add to a local database
-            environ['user.name'] = user_info['display-name']
-            environ['user.email'] = user_info['email']
-            environ['user.username'] = username
-            environ['user.first-name'] = user_info['first-name']
-            environ['user.last-name'] = user_info['last-name']
-
-        # Set a cookie in the user's browser by returning the correct header instruction
-        cookie_val = "%s" % token
-        set_cookie = '%s=%s; Path=/;' % (self._cookie_name, cookie_val)
-        return [(str('Set-Cookie'), str(set_cookie))]
+            self._user_service.create(username,
+                                      user_info['first-name'],
+                                      user_info['last-name'],
+                                      user_info['email'],
+                                      None)
+        return None
 
     def forget(self, environ, identity):
-        """Reverse of remember, will remove any reference to a previously-active
-        Crowd Session
-            Params:
-                environ: WSGI environment
-                identity: User info (not used)
+        """We are relying on the username details being sent in via 
+            an authentication proxy. As such we can not forget them.
+
+            Just empty array
         """
-
-        # Pull out the session token
-        token = self._get_token_value(environ)
-
-        if token:
-            # Ask Crowd to invalidate this session
-            self._client.delete_session(token)
-
-        # return a expires Set-Cookie header to the browser
-        expired = ('%s=""; Path=/; Expires=Sun, 10-May-1971 11:59:00 GMT' %
-                   self._cookie_name)
-        return [('Set-Cookie', expired)]
-
-    def _get_token_value(self, environ):
-        """Gets the token value stored in the cookie"""
-
-        from paste.request import get_cookies
-        cookies = get_cookies(environ)
-        cookie = cookies.get(self._cookie_name)
-
-        if cookie is None:
-            return None
-
-        token = cookie.value
-
-        return token
+        return []
 
 def crowd_challenge_decider(environ, status, headers):
     """Inspects each request and decides whether to challenge for authentication
